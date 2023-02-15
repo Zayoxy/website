99058c3ab67e3c77baf5249efc5511d386c7a85e
Zayoxy <zayoxy@pm.me>
2023-02-15 16:23:07 +0100

    Added all the old assets & began to restructure the source files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+import NavBar from "@/components/NavBar.vue";
+import SocialMedias from "@/components/SocialMedias.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <NavBar />
+    <SocialMedias />
   </header>
 
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/SocialMedias.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+<template>
+  <ul id="socialmedias" class="header-list">
+    <li>
+      <a target="_blank" href="https://github.com/Zayoxy">
+        <img class="glyphs" src="@/assets/images/github.png" alt="github" />
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://twitter.com/Zayoxy">
+        <img class="glyphs" src="@/assets/images/twitter.svg" alt="twitter" />
+      </a>
+    </li>
+    <li>
+      <a
+        target="_blank"
+        href="https://www.youtube.com/channel/UCXPqhJeWX9XWMD61w5kcQfg"
+      >
+        <img class="glyphs" src="@/assets/images/youtube.png" alt="youtube" />
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://twitch.tv/Zayoxy">
+        <img class="glyphs" src="@/assets/images/twitch.svg" alt="twitch" />
+      </a>
+    </li>
+  </ul>
+</template>
